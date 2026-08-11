56f8788fe4030f5b982d7e7b9cc3f331f598e4c7
Ryan Riley <ryanriley@live.com>
2026-08-10 22:13:23 -0500

    feat(nvim): configure Ionide-vim LSP backend, formatting keymaps
    Bare plugin declaration gave syntax highlighting only. Set backend=nvim,
    point at globally-installed fsautocomplete, enable linter/analyzers, and
    wire LspAttach keymaps (gd/K/gr/rn/ca/format).
    
    Also picks up lazyvim.json's omnisharp->dotnet extra swap and the
    resulting lockfile update.

--- nvim/lua/plugins/ionide.lua
+++ nvim/lua/plugins/ionide.lua
@@ -1,4 +1,31 @@
 return {
   "ionide/Ionide-vim",
-  event = "LazyFile",
+  ft = { "fsharp", "fsharp_project" },
+  init = function()
+    vim.g["fsharp#backend"] = "nvim"
+    vim.g["fsharp#fsautocomplete_command"] = { "fsautocomplete" }
+    vim.g["fsharp#lsp_auto_setup"] = 1
+    vim.g["fsharp#lsp_codelens"] = 1
+    vim.g["fsharp#linter"] = 1
+    vim.g["fsharp#unused_opens_analyzer"] = 1
+    vim.g["fsharp#unused_declarations_analyzer"] = 1
+    vim.g["fsharp#automatic_workspace_init"] = 1
+    vim.g["fsharp#automatic_reload_workspace"] = 1
+  end,
+  config = function()
+    vim.api.nvim_create_autocmd("LspAttach", {
+      pattern = { "*.fs", "*.fsi", "*.fsx" },
+      callback = function(args)
+        local opts = { buffer = args.buf }
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+        vim.keymap.set("n", "<leader>cf", function()
+          vim.lsp.buf.format({ async = true })
+        end, opts)
+      end,
+    })
+  end,
 }
